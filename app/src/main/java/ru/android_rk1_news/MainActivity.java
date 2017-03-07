f370b28dc07d82d5a61db5a800d1ec09541c737c
rashemihmih <mihmih96@gmail.com>
2017-03-07 16:48:30 +0300

    create project

---
+++ app/src/main/java/ru/android_rk1_news/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.android_rk1_news;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
