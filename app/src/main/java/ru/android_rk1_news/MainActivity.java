873fad0a5f7d58ab7ada97b08c4e32fd0737f8e5
rashemihmih <mihmih96@gmail.com>
2017-03-07 21:07:39 +0300

    optimized imports

--- app/src/main/java/ru/android_rk1_news/MainActivity.java
+++ app/src/main/java/ru/android_rk1_news/MainActivity.java
@@ -2,7 +2,6 @@ package ru.android_rk1_news;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
--- app/src/test/java/ru/android_rk1_news/ExampleUnitTest.java
+++ app/src/test/java/ru/android_rk1_news/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package ru.android_rk1_news;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
